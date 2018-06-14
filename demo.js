8c4189f9f94d1fdb6efeeaa73e91563fe32cfe95
chenxinda <chenxinda520@qq.com>
2018-06-14 22:25:32 +0800

    这次是关于文件内写入内容的更改

--- demo.js
+++ demo.js
@@ -5,7 +5,6 @@ if(dirName){
     process.chdir("./" + dirName) 
     fs.mkdirSync('css') 
     fs.mkdirSync('js') 
-    
     fs.writeFileSync("./index.html", " <!DOCTYPE><title>Hello</title><h1>Hi</h1>")
     fs.writeFileSync("css/style.css", "h1{color: red;}")
     fs.writeFileSync("./js/main.js", " var string = 'Hello World'alert(string)")
